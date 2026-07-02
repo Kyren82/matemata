231d7c96639e2cd80ccab5987e717c5dbbd0d698
Kyren82 <erykkal123@gmail.com>
2026-07-02 10:04:22 +0200

    file .html: added li response button, responsive-script.js: fixed, theme.css repaired because of script

--- scripts/responsive-script.js
+++ scripts/responsive-script.js
@@ -1,13 +1,48 @@
  const MIN_WIDTH = 768;
+let LiList = document.querySelectorAll(".nav li");
+let HeaderTitle = document.querySelector(".header-title");
+        function ResponseHeaderTitle(){
+            (window.innerWidth < MIN_WIDTH) ? HeaderTitle.innerHTML = "Matemata" : HeaderTitle.innerHTML = "Matemata - kursy dla każdego";
+        }
 
-        function checkWindowWidth() {
-            if (window.innerWidth < MIN_WIDTH) {
-                document.querySelector(".header-title").innerHTML = "Matemata";
-            } else {
-                document.querySelector(".header-title").innerHTML = "Matemata - kursy dla każdego";
-
+        function ResponseMenu(){
+            if(window.innerWidth < MIN_WIDTH){
+                LiList.forEach(el => {
+                    el.classList.add("nav-li-none");
+                });
+                document.querySelector(".first-nav-li").style.display = 'block';
+            }
+            else{
+                LiList.forEach(el => {
+                    el.classList.remove("nav-li-none");
+                });
+                document.querySelector(".first-nav-li").style.display = 'none';
             }
         }
 
-        window.addEventListener('resize', checkWindowWidth);
-        window.addEventListener('DOMContentLoaded', checkWindowWidth);
+
+ResponseHeaderTitle();
+window.addEventListener('resize', ResponseHeaderTitle);
+ResponseMenu();
+window.addEventListener('resize', ResponseMenu);
+
+let clicked = true;
+document.querySelector(".first-nav-li button").addEventListener("click", (e)=>{
+    if(clicked){
+        LiList.forEach((el, index) => {
+            setTimeout(() => {
+                el.classList.remove("nav-li-none");
+            }, index * 50);
+        });
+        clicked = false;
+    }
+    else{   
+        LiList.forEach((el, index) => {
+            setTimeout(() => {
+                el.classList.add("nav-li-none");
+            }, (LiList.length - 1 - index) * 100);
+        });
+        document.querySelector(".first-nav-li").style.display = 'block';
+        clicked = true;
+    }
+});
