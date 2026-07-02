1f5496ea18d79360240b8455a146eb5f97acb45c
Kyren82 <erykkal123@gmail.com>
2026-07-02 15:22:51 +0200

    Small hotfixes

--- scripts/responsive-script.js
+++ scripts/responsive-script.js
@@ -1,25 +1,25 @@
- const MIN_WIDTH = 768;
+const MIN_WIDTH = 768;
 let LiList = document.querySelectorAll(".nav li");
 let HeaderTitle = document.querySelector(".header-title");
 
-        function ResponseHeaderTitle(){
-            (window.innerWidth < MIN_WIDTH) ? HeaderTitle.innerHTML = "Matemata" : HeaderTitle.innerHTML = "Matemata - kursy dla każdego";
-        }
+function ResponseHeaderTitle(){
+    (window.innerWidth < MIN_WIDTH) ? HeaderTitle.innerHTML = "Matemata" : HeaderTitle.innerHTML = "Matemata - kursy dla każdego";
+}
 
-        function ResponseMenu(){
-            if(window.innerWidth < MIN_WIDTH){
-                LiList.forEach(el => {
-                    el.classList.add("nav-li-none");
-                });
-                document.querySelector(".first-nav-li").style.display = 'block';
-            }
-            else{
-                LiList.forEach(el => {
-                    el.classList.remove("nav-li-none");
-                });
-                document.querySelector(".first-nav-li").style.display = 'none';
-            }
-        }
+function ResponseMenu(){
+    if(window.innerWidth < MIN_WIDTH){
+        LiList.forEach(el => {
+            el.classList.add("nav-li-none");
+        });
+        document.querySelector(".first-nav-li").style.display = 'block';
+    }
+    else{
+        LiList.forEach(el => {
+            el.classList.remove("nav-li-none");
+        });
+        document.querySelector(".first-nav-li").style.display = 'none';
+    }
+}
 
 ResponseHeaderTitle();
 window.addEventListener('resize', ResponseHeaderTitle);
